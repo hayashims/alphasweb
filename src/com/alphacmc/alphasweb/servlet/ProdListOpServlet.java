3c9e67ab1fc3229e2e342f9056f0acb6e8a43597
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-05-16 21:43:29 +0900

    add Dao instance

--- src/com/alphacmc/alphasweb/servlet/ProdListOpServlet.java
+++ src/com/alphacmc/alphasweb/servlet/ProdListOpServlet.java
@@ -3,16 +3,23 @@ package com.alphacmc.alphasweb.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alphacmc.alphasweb.dao.ProdDao;
 
+@WebServlet("/prodListop")
 public class ProdListOpServlet  extends HttpServlet {
-	
+
     // DAO
     private ProdDao prodDao = null;
+
+
+    public ProdListOpServlet() {
+    	prodDao = new ProdDao();
+    }
 
     /**
     *
@@ -28,7 +35,7 @@ public class ProdListOpServlet  extends HttpServlet {
        for (String deleleMark : deleleMarks) {
     	   // delete SQL
     	   String deleteSql = deleteSqlPart + deleleMark;
-    	   
+           System.out.println("SQL=" + deleteSql);
     	   prodDao.executeSQL(deleteSql);
        }
 
